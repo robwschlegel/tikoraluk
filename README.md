0c1d1d6ffb7e8f845140fd70f9504655eef9e953
robwschlegel <robwschlegel@gmail.com>
2018-09-28 17:00:55 -0300

    Added a function for calculating MHWs from nested data

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Code for use on tikoraluk
 
 * September 28, 2019
-  * Added script for loading OISST data saved in MATLAB files
+  * Added a function for loading OISST data saved in MATLAB files
+  * Added a function for calculating MHWs from nested data
--- load_mat.R
+++ load_mat.R
@@ -5,6 +5,7 @@
 
 library(tidyverse)
 library(R.matlab)
+library(heatwaveR)
 # library(anytime)
 
 
@@ -24,22 +25,24 @@ load_OISST_mat <- function(df){
     select(lon, lat, date, temp) # ~2 seconds
 }
 
-file_list <- dir("../../oliver/data/sst/noaa_oi_v2/avhrr/timeseries/", full.names = T)
-system.time(
-mat_file <- readMat(file_list[1])
-) # 7 seconds
-system.time(
-mat_file_ts <- as.data.frame(t(mat_file$sst.ts)) %>% 
-  setNames(., as.numeric(mat_file$lat)) %>% 
-  mutate(date = as.Date(as.POSIXct((mat_file$time - 719529) * 86400,
-                           origin = "1970-01-01", tz = "UTC"))) %>% 
-    gather(-date, key = lat, value = temp) %>% 
-    mutate(lon = mat_file$lon[1],
-           lat = as.numeric(lat),
-           temp = ifelse(is.nan(temp), NA, temp)) %>%
-    select(lon, lat, date, temp)
-) # 2 seconds
+# Run MHW algorithm on nested data
+df <- OISST %>% 
+  unnest(sst)
 
+detect_event_event <- function(df){
+  res <- detect_event(ts2clm(df, climatologyPeriod = c("1982-01-01", "2011-12-31")))$event
+}
+
+detect_event_nested <- function(df){
+  res <- df %>% 
+    na.omit() %>% 
+    group_by(lon, lat) %>% 
+    dplyr::rename(t = date) %>% 
+    nest() %>% 
+    mutate(event = map(data, detect_event_event)) %>% 
+    select(-data) %>%
+    unnest()
+}
 
 # Visual test -------------------------------------------------------------
 
@@ -51,3 +54,20 @@ mat_file_ts <- as.data.frame(t(mat_file$sst.ts)) %>%
 
 file_list <- data.frame(files = dir("../../oliver/data/sst/noaa_oi_v2/avhrr/timeseries/", full.names = T),
                         x = 1:length(dir("../../oliver/data/sst/noaa_oi_v2/avhrr/timeseries/", full.names = T)))
+
+system.time(
+OISST <- file_list %>% 
+  slice(5) %>% 
+  mutate(file = x) %>% 
+  nest(-file) %>% 
+  mutate(sst = map(data, load_OISST_mat)) %>% 
+  select(-data)
+)
+
+system.time(
+OISST_event <- OISST %>%
+  mutate(event = map(sst, detect_event_nested)) %>% 
+  select(event) %>%
+  unnest()
+) # 100 seconds
+
