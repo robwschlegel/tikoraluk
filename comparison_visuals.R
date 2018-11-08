74551b026350c91e537ac5590976dd86cf50389b
robwschlegel <robwschlegel@gmail.com>
2018-11-08 15:45:11 -0400

    A bit more work

--- comparison_visuals.R
+++ comparison_visuals.R
@@ -15,9 +15,13 @@ load("metadata/lon_OISST.RData")
 
 # Data --------------------------------------------------------------------
 
+# The various comparison results
 load("../data/OISST_NAPA_difference.RData")
 load("../data/OISST_NAPA_correlation.RData")
+load("../data/AVISO_NAPA_skewness_diff.RData")
+load("../data/OISST_NAPA_MHW_summary.RData")
 
+# A custom palette
 pretty_palette <- c("#fefefe", "#f963fa", "#020135", "#00efe1", "#057400", "#fcfd00", "#ed0000", "#3d0000")
 
 # The base map
@@ -31,14 +35,14 @@ map_base <- ggplot2::fortify(maps::map(fill = TRUE, plot = FALSE)) %>%
 # Functions ---------------------------------------------------------------
 
 # Plotting function
-polar_map_diff <- function(sum_stat, df, plot_name = NA){
+polar_map_diff <- function(sum_stat, df, plot_name = NA, chosen_palette = "red-blue"){
   pp <- ggplot() + theme_void() +
     geom_point(data = df,
                aes_string(x = "-nav_lon", y = "-nav_lat", colour = sum_stat), size = 0.001) +
     geom_polygon(data = map_base, aes(x = -lon, y = -lat, group = group)) +
     # scale_colour_viridis_c() +
     # scale_color_distiller(palette = "Spectral") +
-    scale_colour_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
+    # scale_colour_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
     # scale_colour_gradientn(colours = pretty_palette) +
     coord_polar() +
     labs(x = "", y = "", title = sum_stat) +
@@ -50,95 +54,133 @@ polar_map_diff <- function(sum_stat, df, plot_name = NA){
           plot.title = element_text(hjust = 0.5, size = 30), 
           strip.text = element_text(size = 16)) +
     facet_wrap(~month, ncol = 3)
+  if(chosen_palette == "red-blue"){
+    pp_col <- pp + scale_colour_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0)
+  } else if(chosen_palette == "viridis"){
+    pp_col <- pp + scale_colour_viridis_c()
+  } else if(chosen_palette == "spectral"){
+    pp_col <- pp + scale_color_distiller(palette = "Spectral")
+  }else if(chosen_palette == "pretty"){
+    pp_col <- pp + scale_colour_gradientn(colours = pretty_palette)
+  }
   if(is.na(plot_name)) plot_name <- sum_stat
-  ggsave(pp, filename = paste0("graph/diff_figs/",plot_name,".png"), width = 12, height = 21)
+  ggsave(pp_col, filename = paste0("graph/diff_figs/",plot_name,".png"), width = 12, height = 21)
 }
 #
 
+
 # Full visuals for one summary stat ---------------------------------------
 
-# Remove NA and 0 rows for now...
-diff_res_na_omit <- OISST_NAPA_difference %>% 
-  na.omit() %>% 
-  filter(min_NAPA != 0)
+# # Remove NA and 0 rows for now...
+# diff_res_na_omit <- OISST_NAPA_difference %>% 
+#   na.omit() %>% 
+#   filter(min_NAPA != 0)
+# 
+# # Plotting function
+# polar_map <- function(the_chosen, diff_val = F){
+#   pp <- ggplot() + theme_void() +
+#     geom_polygon(data = map_base, aes(x = -lon, y = -lat, group = group)) +
+#     geom_point(data = filter(diff_res_na_omit, month == "overall"),
+#                aes_string(x = "-nav_lon", y = "-nav_lat", colour = the_chosen), size = 0.001) +
+#     # scale_colour_viridis_c() +
+#     scale_colour_gradientn(colours = pretty_palette) +
+#     coord_polar() +
+#     # scale_y_continuous(limits = c(-90, -25)) +
+#     labs(x = "", y = "") +
+#     theme(legend.position = c(0.8, 0.5),
+#           legend.background = element_blank(),
+#           legend.title = element_blank(),
+#           legend.text = element_text(colour = "white"),
+#           axis.text = element_blank()#,
+#           # axis.ticks = element_blank(),
+#           # plot.background = element_blank(),
+#           # panel.background = element_blank(),
+#           # plot.title = element_text(hjust = 0.5, vjust = 1),
+#           # axis.line = element_blank()
+#     )
+#   if(diff_val){
+#     suppressMessages(
+#       pp <- pp + scale_colour_gradient2(low = "blue", 
+#                                         mid = "white",
+#                                         high = "red",
+#                                         midpoint = 0)
+#     )
+#   }
+#   return(pp)
+# }
+# 
+# # Make some pretty pictures
+# napa_map <- polar_map("quant_50_NAPA")
+# napa_title <- text_grob(label = "NAPA median", face = "bold",
+#                         size = 16, just = c("center","center"))
+# oisst_map <- polar_map("quant_50_OISST")
+# oisst_title <- text_grob(label = "OISST median",  face = "bold",
+#                          size = 16, just = c("center","center"))
+# diff_map <- polar_map("quant_50_diff", diff_val = T)
+# diff_title <- text_grob(label = "NAPA - OISST", face = "bold",
+#                         size = 16, just = c("center","center"))
+# 
+# tri_plot <- ggplot() +
+#   theme_void() +
+#   geom_blank() +
+#   scale_x_continuous(limits = c(0.1, 2.9)) +
+#   scale_y_continuous(limits = c(0.1,0.9)) +
+#   annotation_custom(grob = ggplotGrob(oisst_map),
+#                     xmin = -0.1, xmax = 1.1,
+#                     ymin = 0, ymax = 1) +
+#   annotation_custom(grob = oisst_title,
+#                     xmin = -0.1, xmax = 1.1,
+#                     ymin = 0.8, ymax = 0.9) +
+#   annotation_custom(grob = ggplotGrob(napa_map),
+#                     xmin = 0.9, xmax = 2.1,
+#                     ymin = 0, ymax = 1) +
+#   annotation_custom(grob = napa_title,
+#                     xmin = 0.9, xmax = 2.1,
+#                     ymin = 0.8, ymax = 0.9) +
+#   annotation_custom(grob = ggplotGrob(diff_map),
+#                     xmin = 1.9, xmax = 3.1,
+#                     ymin = 0, ymax = 1) +
+#   annotation_custom(grob = diff_title,
+#                     xmin = 1.9, xmax = 3.1,
+#                     ymin = 0.8, ymax = 0.9)
+# 
+# # ggarrange(oisst_map, napa_map, diff_map, nrow = 1, ncol = 3)
+# # grid.arrange(napa_title, napa_map, ncol = 1, heights = c(2,15)) 
+# ggsave(plot = tri_plot, filename = "graph/median_diff.png", height = 6, width = 16)
 
-# The base map
-map_base <- ggplot2::fortify(maps::map(fill = TRUE, plot = FALSE)) %>% 
-  dplyr::rename(lon = long) %>% 
-  filter(lat >= 25.6, lon <= 180) #%>% 
-# mutate(lon = ifelse(lon > 180, lon-180, lon))
 
-# Plotting function
-polar_map <- function(the_chosen, diff_val = F){
-  pp <- ggplot() + theme_void() +
-    geom_polygon(data = map_base, aes(x = -lon, y = -lat, group = group)) +
-    geom_point(data = filter(diff_res_na_omit, month == "overall"),
-               aes_string(x = "-nav_lon", y = "-nav_lat", colour = the_chosen), size = 0.001) +
-    # scale_colour_viridis_c() +
-    scale_colour_gradientn(colours = pretty_palette) +
-    coord_polar() +
-    # scale_y_continuous(limits = c(-90, -25)) +
-    labs(x = "", y = "") +
-    theme(legend.position = c(0.8, 0.5),
-          legend.background = element_blank(),
-          legend.title = element_blank(),
-          legend.text = element_text(colour = "white"),
-          axis.text = element_blank()#,
-          # axis.ticks = element_blank(),
-          # plot.background = element_blank(),
-          # panel.background = element_blank(),
-          # plot.title = element_text(hjust = 0.5, vjust = 1),
-          # axis.line = element_blank()
-    )
-  if(diff_val){
-    suppressMessages(
-      pp <- pp + scale_colour_gradient2(low = "blue", 
-                                        mid = "white",
-                                        high = "red",
-                                        midpoint = 0)
-    )
-  }
-  return(pp)
-}
+# Distance visual ---------------------------------------------------------
 
-# Make some pretty pictures
-napa_map <- polar_map("quant_50_NAPA")
-napa_title <- text_grob(label = "NAPA median", face = "bold",
-                        size = 16, just = c("center","center"))
-oisst_map <- polar_map("quant_50_OISST")
-oisst_title <- text_grob(label = "OISST median",  face = "bold",
-                         size = 16, just = c("center","center"))
-diff_map <- polar_map("quant_50_diff", diff_val = T)
-diff_title <- text_grob(label = "NAPA - OISST", face = "bold",
-                        size = 16, just = c("center","center"))
+only_water <- OISST_NAPA_correlation %>% 
+  select(nav_lon, nav_lat) %>% 
+  unique()
 
-tri_plot <- ggplot() +
-  theme_void() +
-  geom_blank() +
-  scale_x_continuous(limits = c(0.1, 2.9)) +
-  scale_y_continuous(limits = c(0.1,0.9)) +
-  annotation_custom(grob = ggplotGrob(oisst_map),
-                    xmin = -0.1, xmax = 1.1,
-                    ymin = 0, ymax = 1) +
-  annotation_custom(grob = oisst_title,
-                    xmin = -0.1, xmax = 1.1,
-                    ymin = 0.8, ymax = 0.9) +
-  annotation_custom(grob = ggplotGrob(napa_map),
-                    xmin = 0.9, xmax = 2.1,
-                    ymin = 0, ymax = 1) +
-  annotation_custom(grob = napa_title,
-                    xmin = 0.9, xmax = 2.1,
-                    ymin = 0.8, ymax = 0.9) +
-  annotation_custom(grob = ggplotGrob(diff_map),
-                    xmin = 1.9, xmax = 3.1,
-                    ymin = 0, ymax = 1) +
-  annotation_custom(grob = diff_title,
-                    xmin = 1.9, xmax = 3.1,
-                    ymin = 0.8, ymax = 0.9)
+lon_lat_NAPA_OISST_no_land <- left_join(only_water, lon_lat_NAPA_OISST, 
+                                        by = c("nav_lon", "nav_lat")) %>% 
+  arrange(dist)
 
-# ggarrange(oisst_map, napa_map, diff_map, nrow = 1, ncol = 3)
-# grid.arrange(napa_title, napa_map, ncol = 1, heights = c(2,15)) 
-ggsave(plot = tri_plot, filename = "graph/median_diff.png", height = 6, width = 16)
+# Map of distance between pixels
+dp <- ggplot() + theme_void() +
+  geom_polygon(data = map_base, aes(x = -lon, y = -lat, group = group)) +
+  geom_point(data = lon_lat_NAPA_OISST_no_land,
+             aes(x = -nav_lon, y = -nav_lat, colour = dist), size = 0.001) +
+  # scale_colour_viridis_c() +
+  scale_color_distiller(palette = "BuPu") +
+  # scale_colour_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
+  coord_polar() +
+  labs(x = "", y = "", title = "Distance (km) between nearest pixels") +
+  theme(legend.position = "bottom",
+        legend.background = element_blank(),
+        legend.title = element_blank(),
+        legend.text = element_text(colour = "black"),
+        axis.text = element_blank(),
+        plot.title = element_text(hjust = 0.5))
+ggsave(dp, filename = "graph/diff_figs/distance.png", width = 4, height = 5)
+rm(dp)
+
+# Histogram of distanes between pixels
+
+# Line graphs showing relationship between summary stats and pixel distance
 
 
 # Difference visuals ------------------------------------------------------
@@ -191,43 +233,8 @@ ggsave(cp, filename = "graph/diff_figs/correlation.png", width = 12, height = 21
 rm(cp)
 
 
-# Distance visual ---------------------------------------------------------
-
-only_water <- OISST_NAPA_correlation %>% 
-  select(nav_lon, nav_lat) %>% 
-  unique()
-
-lon_lat_NAPA_OISST_no_land <- left_join(only_water, lon_lat_NAPA_OISST, 
-                                        by = c("nav_lon", "nav_lat")) %>% 
-  arrange(dist)
-
-# Map of distance between pixels
-dp <- ggplot() + theme_void() +
-  geom_polygon(data = map_base, aes(x = -lon, y = -lat, group = group)) +
-  geom_point(data = lon_lat_NAPA_OISST_no_land,
-             aes(x = -nav_lon, y = -nav_lat, colour = dist), size = 0.001) +
-  # scale_colour_viridis_c() +
-  scale_color_distiller(palette = "BuPu") +
-  # scale_colour_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
-  coord_polar() +
-  labs(x = "", y = "", title = "Distance (km) between nearest pixels") +
-  theme(legend.position = "bottom",
-        legend.background = element_blank(),
-        legend.title = element_blank(),
-        legend.text = element_text(colour = "black"),
-        axis.text = element_blank(),
-        plot.title = element_text(hjust = 0.5))
-ggsave(dp, filename = "graph/diff_figs/distance.png", width = 4, height = 5)
-rm(dp)
-
-# Histogram of distanes between pixels
-
-# Line graphs showing relationship between summary stats and pixel distance
-
-
 # Skewness visual ---------------------------------------------------------
 
-load("../data/AVISO_NAPA_skewness_diff.RData")
 polar_map_diff("skewness", filter(AVISO_NAPA_skewness_diff, product == "difference"))
 polar_map_diff("skewness", filter(AVISO_NAPA_skewness_diff, product == "AVISO"), "skewness_AVISO")
 polar_map_diff("skewness", filter(AVISO_NAPA_skewness_diff, product == "NAPA"), "skewness_NAPA")
@@ -235,7 +242,6 @@ polar_map_diff("skewness", filter(AVISO_NAPA_skewness_diff, product == "NAPA"), 
 
 # MHW difference visuals --------------------------------------------------
 
-load("../data/OISST_NAPA_MHW_summary.RData")
 OISST_NAPA_MHW_summary_diff <- filter(OISST_NAPA_MHW_summary, product == "difference")
 
 # Visuals for the clim stat differences
