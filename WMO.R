57c9d79fe71af1d42fd9f90b459a9e90796a2502
robwschlegel <robwschlegel@gmail.com>
2022-01-17 14:40:48 -0400

    2021 end of year WMO text edit

--- WMO.R
+++ WMO.R
@@ -19,6 +19,9 @@ MHW_cat_daily_2016 <- readRDS("../MHWapp/data/annual_summary/OISST_cat_daily_198
 # Load the most coverage year
 MCS_cat_daily_1985 <- readRDS("../MHWapp/data/annual_summary/OISST_MCS_cat_daily_1982-2011_1985.Rds")
 
+# Load the most recent low coverage year
+MHW_cat_daily_2012 <- readRDS("../MHWapp/data/annual_summary/OISST_cat_daily_1982-2011_2012.Rds")
+
 # Load 2019
 MHW_cat_daily_2019 <- readRDS("../MHWapp/data/annual_summary/OISST_cat_daily_1982-2011_2019.Rds")
 MCS_cat_daily_2019 <- readRDS("../MHWapp/data/annual_summary/OISST_MCS_cat_daily_1982-2011_2019.Rds")
@@ -81,6 +84,7 @@ first_cum_prop_stats <- function(df){
 }
 
 # Get MHW stats
+first_cum_prop_stats(MHW_cat_daily_2012)
 first_cum_prop_stats(MHW_cat_daily_2016)
 first_cum_prop_stats(MHW_cat_daily_2020)
 first_cum_prop_stats(MHW_cat_daily_2021)
@@ -142,77 +146,67 @@ In total, 84% of the ocean experienced at least one MHW (Figure 7C), matching th
 On average, each ocean pixel experienced a total of 77 MHW days (Figure 7D). 
 This is greater than 2019 (74) but less than the 2016 peak (83)."
 
-# 2021 MHW text
-"MARINE HEATWAVES
-As with heatwaves on land, extreme heat can affect the near-surface layer of the oceans. 
-This situation is called a marine heatwave (MHW), and it can cause a range of consequences for marine life and dependent communities. 
-Satellite retrievals of sea-surface temperature can be used to monitor MHWs.  
-An MHW is categorized here as moderate, strong, severe or extreme (for definitions, see Marine heatwave data).
+# 2021 text
+"Marine heatwaves and cold-spells
+In analogy with heatwaves and cold spells on land, marine heatwaves (MHW) and marine cold-spells (MCS) are prolonged periods of 
+extreme heat or cold that affect the near-surface layer of the ocean. 
+They can have a range of consequences for marine life and dependent communities and MHWs have become more frequent over the 20th Century. 
+Satellite retrievals of sea-surface temperature are used to monitor MHWs and MCSs, categorized here as moderate, strong, severe, or
+extreme (for definitions, seeMarine heatwave and marine cold spell data).
 
-Much of the ocean experienced at least one ‘strong’ MHW at some point in 2021 (Figure XXa). 
-Conspicuously absent are MHWs in the eastern equatorial Pacific Ocean and much of the Southern Ocean. 
-The period of sea-ice cover in the Southern Ocean has been extending later into the year.
-This is one of the very few parts of the global ocean where marine cold-spells (MCSs) are increasing in duration.
-A continuation of MHWs occurring at the end of 2020, the Laptev to Beaufort Sea's experienced particularly intense MHWs from January to April. 
-The average daily coverage of the global ocean by MHWs in 2020 was 13% (Figure  XXb).
-This percentage is less that of 2020 (16%) and the 2016 peak of 17%. 
-The ocean experienced a similar amount of MHWs classified as ‘strong’ (25%) and ‘moderate’ (25%).
-In total, 55% of the surface ocean experienced at least one MHW during 2021 (Figure  XXc);
-this is less than the percentage of the ocean that experienced MHWs in 2020 (63%) and the 2016 peak (65%).
-Note that many of these numbers differ slightly from previous reports because the calculations are now weighted on the surface area of the SST pixels.
-Previous reports weighted all pixels the same, which allowed the polar regions to have an outsized weight in the results."
+Much of the ocean experienced at least one 'strong' MHW at some point in 2021 (Figure 7).
+Due to the below average sea-surface temperatures associated with a strong La Niña year, MHWs were conspicuously absent in the 
+eastern equatorial Pacific Ocean, which was also one of the only regions of the global ocean to see broad MCS coverage.
+The Southern Ocean is one of the only areas in which MCSs are increasing in duration.
+The Laptev and Beaufort Seas experienced 'severe' and 'extreme' MHWs from January to April 2021.
+The ice-edge regions to the east of Greenland (August), north of Svalbard (October), and east of the Ross Sea (December) experienced notable 'extreme' MHWs.
+In 2021, almost all MCSs were 'moderate', except in areas of high variability such as the poleward extension of the Gulf Stream.
 
-# 2021 MHW text
-"MARINE COLD-SPELLS
-As with cold-spells on land, extreme cold can affect the near-surface layer of the oceans. 
-This situation is called a marine cold-spell (MCS), and it can cause a range of consequences for marine life and dependent communities. 
-Satellite retrievals of sea-surface temperature can be used to monitor MCSs.  
-An MCS is categorized here the same as MHWs, e.g. moderate, strong, severe or extreme (for definitions, see Marine heatwave data).
+MHWs in 2021 showed an average daily coverage of 13%, which is less than the record of 17% in 2016 and 16% in 2020.
+The most common category of MHW in 2021 was ‘strong’ (28%) for the eighth consecutive year.
+Overall 57% of the ocean surface experienced at least one MHW during 2021 (Figure 7c) – less than the record of 65% in 2016 
+and the lowest annual coverage since 2012 (57%).
 
-The occurrence of MCSs in the global ocean was much less common than MHWs for 2021 (Figure XYa, Figure XXa). 
-Almost all MCSs that did occur in 2021 were 'moderate', with the one consistent exception being the eddies that spin off of 
-the poleward extensions of western boundary currents (WBC), which are generally classified as 'extreme' events.
-The eastern equatorial Pacific Ocean, which experienced almost no MHWs in 2021, was one of the few non-polar regions of the global ocean to see broad MCS coverage.
-The average daily coverage of the global ocean by MCSs in 2020 was 3% (Figure  XYb).
-This percentage is less that of 2020 (4%) and half that of the 1982 peak of 7%. 
-The ocean experienced MCSs predominantly classified as ‘moderate’ (17%) with ‘strong’ (3%) the second highest by some distance.
-In total, 22% of the surface of the ocean experienced at least one MCS during 2021 (Figure  XYc);
-this is less than percentage of the ocean that experienced MCSs in 2020 (25%) and the 1985 peak (63%).
-Note that because MCSs now occur almost exclusively in the polar regions of the ocean, 
-the decision to calculate global statistics based on pixels weighted by surface area can have a large effect because polar pixels 
-are relatively much smaller than their equatorial counterparts."
+The average daily coverage of the global ocean by MCSs in 2021 was 4% (Figure 8b) –
+a lower value than the record high in 1982 (7%) and comparable to 2020 (4%). 
+In total, 25% of the ocean surface experienced at least one MCS during 2021 (Figure 8c), 
+which is comparable to 2020 (25%), but much less than the 1985 record (63%). 
+Of the 25% coverage, most MCS were classified as either ‘moderate’ (20%) or ‘strong’ (4%)."
 
 # MHW figure caption
-"Figure 8. (a) Global map showing the highest MHW category (for definitions, see Marine heatwave data) experienced at each pixel 
-over the course of the year (reference period 1982–2011). Light grey indicates that no MHW occurred in a pixel over the entire year; 
+"Figure 7: (a) Global map showing the highest MHW category (for definitions, see Marine heatwave and marine cold-spell data) 
+experienced at each pixel over 2021 (reference period 1982–2011). 
+Light grey indicates that no MHW occurred in a pixel over the entire year; 
 (b) Stacked bar plot showing the percentage of the surface of the ocean experiencing an MHW on any given day of the year; 
 (c) Stacked bar plot showing the cumulative percentage of the surface of the ocean that experienced an MHW over the year. 
-Note: These values are based on when in the year a pixel first experienced its highest MHW category, so no pixel is counted twice.  
+Note: These values are based on when in the year a pixel first experienced its highest MHW category, so no pixel is counted twice.
 Horizontal lines in this figure show the final percentages for each category of MHW; 
-(d) Stacked bar plot showing the cumulative number of MHW days averaged over the surface of the ocean.  
+d) Stacked bar plot showing the cumulative number of MHW days averaged over the surface of the ocean. 
 Note: This average is calculated by dividing the cumulative sum of MHW days per pixel weighted by the surface area of those pixels.
-Source: Robert Schlegel"
+Data are from NOAA OISST. Source: Robert Schlegel"
 
 # MCS figure caption
-## Same as MHW caption
+"Figure 8: as for Figure 7 but showing marine cold-spells (MCSs) rather than marine heatwaves (MHWs). Data are from NOAA OISST.
+Source: Robert Schlegel"
 
 # Addendum
-"MARINE HEATWAVE DATA
-MHWs are categorized as moderate when the sea-surface temperature (SST) is above the
-90th percentile of the climatological distribution for five days or longer; the subsequent
-categories are defined with respect to the difference between the SST and the climatological
-distribution average: strong, severe, or extreme, if that difference is, respectively, more than
-two, three or four times the difference between the 90th percentile and the climatological
-distribution average (Hobday et al., 2018).
+"Marine heatwave and marine cold-spell data
+MHWs are categorized as moderate when the sea-surface temperature (SST) is above the 90th
+percentile of the climatological distribution for five days or longer; the subsequent categories are
+defined with respect to the difference between the SST and the climatological distribution average:
+strong, severe, or extreme, if that difference is, respectively, more than two, three or four times the
+difference between the 90th percentile and the climatological distribution average (Hobday et al.,
+2018). MCS categories are analogous but counting days below the 10th percentile.
 
-The baseline used for MHWs is 1982–2011, which is shifted by one year from the standard
-normal period of 1981–2010 because the satellite SST series on which it is based starts in
-1981.
+The baseline used for MHWs and MCSs is 1982–2011, which is shifted by one year from the
+standard normal period of 1981–2010 because the first full year of the satellite SST series on which
+it is based is 1982.
 
 Hobday, A.J. et al., 2018: Categorizing and Naming Marine Heatwaves. Oceanography, 31(2):
 1–13. doi: https://eprints.utas.edu.au/27875/.
 
 NOAA OISST v2: Optimum Interpolation Sea Surface Temperature (OISST):
+
 Banzon, V. et al., 2016: A Long-Term Record of Blended Satellite and in Situ Sea-Surface
 Temperature for Climate Monitoring, Modeling and Environmental Studies. Earth System
 Science Data, 8(1): 165–176. doi: https://essd.copernicus.org/articles/8/165/2016/."
